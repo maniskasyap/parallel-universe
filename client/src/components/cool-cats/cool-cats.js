6dea9f30c7dab1dd191ab802c3fe9976878573e5
manish <manis.kasyap@gmail.com>
2018-10-10 21:42:11 +0400

    proxying api calls and first component

---
+++ client/src/components/cool-cats/cool-cats.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./cool-cats.css";
+
+class CoolCats extends Component {
+  render() {
+    return (
+      <div>
+        <h2>CoolCats</h2>
+      </div>
+    );
+  }
+}
+
+export default CoolCats;
