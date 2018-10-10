b4a4938061f6177279071471f1bbb3ecb80388ce
manish <manis.kasyap@gmail.com>
2018-10-10 21:58:29 +0400

    running with concurrently and npm scripts

--- client/src/components/cool-cats/cool-cats.js
+++ client/src/components/cool-cats/cool-cats.js
@@ -2,10 +2,36 @@ import React, { Component } from "react";
 import "./cool-cats.css";
 
 class CoolCats extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cats: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("/cats")
+      .then(res => res.json())
+      .then(cats =>
+        this.setState(
+          {
+            cats
+          },
+          () => console.log("cats fetched...", cats)
+        )
+      );
+  }
   render() {
     return (
       <div>
         <h2>CoolCats</h2>
+        <ul>
+          {this.state.cats.map(cat => (
+            <li key={cat._id}>
+              {cat.name} - {cat.style}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
