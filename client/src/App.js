8645251a72fb27cfb24ccc2b0769eaee1574970f
manish <manis.kasyap@gmail.com>
2018-10-11 22:50:06 +0400

    adding react dynamic router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link, Route } from "react-router-dom";
 import "./App.css";
 import CoolCats from "./components/cool-cats/cool-cats";
 
@@ -6,7 +7,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CoolCats />
+        <nav>
+          <Link to="/cats">Cats</Link>
+          <Link to="/dogs">Dogs</Link>
+          <Link to="/bunnies">Bunnies</Link>
+        </nav>
+        <div>
+          <Route path="/cats" component={CoolCats} />
+        </div>
       </div>
     );
   }
